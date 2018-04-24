5e222aa614b63a22ea19cfd7fb6541c77c3ae346
Jake <spacejake@gmail.com>
2018-04-24 22:06:05 +0900

    add data loader for GRID corpus

---
+++ src/data/grid_loader.py
@@ -0,0 +1,156 @@
+from __future__ import print_function
+import os
+import os.path
+import sys
+import torch
+import torch.utils.data as data
+from PIL import Image, ImageDraw
+
+import numpy as np
+
+from os import listdir
+from os import walk
+
+from video.video import Video
+
+'''
+Grid Dataset:
+``dset.GRID(root="dir)``
+'''
+
+class GRID(data.Dataset):
+    """VOC Classification Dataset Object
+
+    input is image, target is annotation
+
+    Arguments:
+        root (string): filepath to VOCdevkit folder.
+        image_set (string): imageset to use (eg. 'train', 'val', 'test')
+        transform (callable, optional): transformation to perform on the
+            input image
+        target_transform (callable, optional): transformation to perform on the
+            target `annotation`
+            (eg: take in caption string, return tensor of word indices)
+        dataset_name (string, optional): which dataset to load
+            (default: 'VOC2007')
+    """
+
+    def __init__(self, root): #, transform=None, target_transform=None):
+        self.root = root
+        self.vid_dir = 'vid'
+        self.anno_dir = 'align'
+        #self.transform = transform
+        #self.target_transform = target_transform
+        self.VID_EXT = "mpg"
+        self.ANNO_EXT = "align"
+        self.ids = []
+        self.id_to_user = {}
+
+        # 1 arg is id
+        self._annopath = os.path.join(
+            self.root, self.anno_dir, '%s.' + self.ANNO_EXT)
+
+        # 1 arg is speaker dir
+        # 2 arg is id
+        self._vidpath = os.path.join(
+            self.root, self.vid_dir, '%s','%s.' + self.VID_EXT )
+
+        # Aid in construction of Ids and video locations,
+
+        video_path = os.path.join(self.root, self.vid_dir)
+        for (dirName, subdirList, filenames) in walk(video_path, topdown=False):
+            print("Found Vid Dir: %s" % dirName)
+            user_dir = os.path.split(dirName)[-1]
+            for fname in filenames:
+                if not fname.endswith('.' + self.VID_EXT):
+                    continue
+
+                id = os.path.splitext(fname)[0]
+                self.id_to_user[id] = user_dir
+
+        print("Map of Users(%s)" % len(self.id_to_user))
+
+        self.ids = list(self.id_to_user.keys())
+        # anno_path = os.path.join(self.root, self.anno_dir)
+        # for fname in listdir(anno_path):
+        #     if fname.endswith('.' + self.ANNO_EXT):
+        #         id = os.path.splitext(fname)[0]
+        #         # check if we have a video associated with annotation
+        #         if id in id_to_user:
+        #             self.ids.append((id, id_to_user[id]))
+
+
+
+    def __getitem__(self, index):
+        data_id = self.ids[index]
+
+        # Needed to find Video Directory
+        user = self.id_to_user[data_id]
+
+        vid_path = self._vidpath % (user, data_id)
+        anno_path = self._annopath % str(data_id)
+
+        print("loading video: %s" % vid_path )
+        print("loading aligns: %s" % anno_path )
+
+        video = Video(vid_path, anno_path)
+
+        #TODO: If we want Transforms for video frames
+        # Set transforms in video and have to it in getItem!!
+        '''
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        '''
+
+        return video
+
+    def __len__(self):
+        return len(self.ids)
+
+
+# def video_collate(batch):
+#     """Custom collate fn for dealing with batches of images that have a different
+#     number of associated object annotations (bounding boxes).
+#
+#     Arguments:
+#         batch: (tuple) A tuple of tensor images and lists of annotations
+#
+#     Return:
+#         A tuple containing:
+#             1) (tensor) batch of images stacked on their 0 dim
+#             2) (list of tensors) annotations for a given image are stacked on 0 dim
+#     """
+#
+#     data = [item[0] for item in batch]
+#     target = [item[1] for item in batch]
+#     target = torch.LongTensor(target)
+#
+#     return [data,target]
+
+def main():
+
+    data_path = '/home/jake/classes/cs703/Project/data/grid/'
+    loader = GRID(data_path)
+
+    count = 0
+
+    for video in loader:
+        # Only testing
+        if count > 10:
+            return
+        count += 1
+        for idx, (frame, word) in enumerate(video):
+            print("Frame: %s Word: %s" % (idx, word))
+            frame.show()
+            # Only testing
+            break
+
+    #cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+
+    main()
---
+++ src/data/video/transcript.py
@@ -0,0 +1,107 @@
+
+class Transcript(object):
+    def __init__(self, path=None):
+
+        if path is None:
+            self.aligns = []
+            self.sentence = None
+        else:
+            self.from_file(path)
+
+    def from_file(self, path):
+        #reset
+        self.sentence = None
+
+        with open(path, 'r') as f:
+            lines = f.readlines()
+        align = [Align(int(y[0])/1000, int(y[1])/1000, y[2]) for y in [x.strip().split(" ") for x in lines]]
+
+        # 'sil' means silence
+        # 'sp' means there is video problem and should be skipped
+        # remove buggy frames, but keep silence?
+        # self.align = self.strip(align, ['sp'])
+        self.aligns = self.strip(align, ["sp", "sil"])
+
+        self.sentence = self.get_sentence()
+
+        return self
+
+    def from_align(self, align):
+        self.aligns.append(align)
+        return self
+
+    def strip(self, aligns, items):
+        return [sub for sub in aligns if sub.word not in items]
+
+    def make_sentence(self, aligns=None):
+        return " ".join([y.word for y in aligns])
+
+    def get_sentence(self):
+        if self.sentence is None:
+            self.sentence = self.make_sentence(self.aligns)
+        return self.sentence
+
+    def get_word_from_frame(self, frame_num):
+        for align in self.aligns:
+            if frame_num >= align.start and frame_num < align.end:
+                return align.word
+
+        return None
+
+    '''
+    # Returns the length of the longest word 
+    '''
+    @property
+    def word_length(self):
+        return len(self.sentence.split(" "))
+
+    '''
+    # Number of first frame 
+    '''
+    @property
+    def first_frame(self):
+        return self.aligns[0].start if len(self.aligns) > 0 else 0
+
+
+    '''
+    # Number of last frame 
+    '''
+    @property
+    def last_frame(self):
+        return self.aligns[-1].end if len(self.aligns) > 0 else 0
+
+    '''
+    Returns total chars in sentence
+    '''
+    @property
+    def sentence_length(self):
+        return len(self.sentence)
+
+class Align(object):
+    def __init__(self, start, end, word):
+        self.start = start
+        self.end   = end
+        self.word  = word
+
+    def __str__(self):
+        return str((self.start, self.end ,self.word))
+
+    __repr__ = __str__
+
+
+def main():
+    path = '/home/jake/classes/cs703/Project/data/grid/anno/align/lgaz8p.align'
+
+    transcript = Transcript(path)
+
+    print(transcript.get_sentence())
+    print(transcript.aligns)
+    print(transcript.sentence_length)
+    print(transcript.word_length)
+    print(transcript.first_frame)
+    print(transcript.last_frame)
+
+if __name__ == '__main__':
+
+    main()
+
