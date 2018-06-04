b1cde6f4a8b75f5fbfecb86cec3da451aaabf725
gunheelee <ivagasm@naver.com>
2018-06-04 17:54:57 +0900

    Train changed

--- src/train_optflow.sh
+++ src/train_optflow.sh
@@ -2,5 +2,5 @@
 
 #python train_optflow_lstm.py --train --dataroot /home/jake/classes/cs703/Project/data/grid/	--features-model /home/jake/classes/cs703/Project/dev/TellGAN/src/assests/predictors/shape_predictor_68_face_landmarks.dat
 
-CUDA_VISIBLE_DEVICES=1 python train_optflow_lstm.py --train --mouth --dataroot /data2/victorleee/grid/ --ckptdir ./optflowGAN_chkpnts --features-model ./shape_predictor_68_face_landmarks.dat
+CUDA_VISIBLE_DEVICES=0 python train_optflow_lstm.py --train --mouth --dataroot /data2/victorleee/grid/ --ckptdir ./optflowGAN_chkpnts --features-model ./shape_predictor_68_face_landmarks.dat
 
--- src/train_optflow_jake.sh
+++ src/train_optflow_jake.sh
@@ -2,5 +2,5 @@
 
 #python train_optflow_lstm.py --train --dataroot /home/jake/classes/cs703/Project/data/grid/	--features-model /home/jake/classes/cs703/Project/dev/TellGAN/src/assests/predictors/shape_predictor_68_face_landmarks.dat
 #python train_optflow_lstm.py --train --continue --ep-start 0 --vid-start 441 --dataroot /home/jake/classes/cs703/Project/data/grid/ --ckptdir ./optflowGAN_chkpnts --features-model /home/jake/classes/cs703/Project/dev/TellGAN/src/assests/predictors/shape_predictor_68_face_landmarks.dat $@
-python train_optflow_lstm.py --train --dataroot /home/jake/classes/cs703/Project/data/grid/ --ckptdir ./optflowGAN_chkpnts --features-model /home/jake/classes/cs703/Project/dev/TellGAN/src/assests/predictors/shape_predictor_68_face_landmarks.dat $@
 
+python train_optflow_lstm.py --train --dataroot /home/jake/classes/cs703/Project/data/grid/ --ckptdir ./optflowGAN_chkpnts --features-model /home/jake/classes/cs703/Project/dev/TellGAN/src/assests/predictors/shape_predictor_68_face_landmarks.dat $@
