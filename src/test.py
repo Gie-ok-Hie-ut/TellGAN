7fd5b4106d1b13ac71876435a468d71bae62ebd6
Jake <spacejake@gmail.com>
2018-06-07 01:02:18 +0900

    Add Test output video

--- src/test.py
+++ src/test.py
@@ -8,10 +8,52 @@ from util import html
 from data.video.transform.localizeface import LocalizeFace, FeaturePredictor
 from data.grid_loader import GRID
 from torchvision import transforms
+import cv2
 import skvideo.io
 from PIL import Image
 import numpy as np
 
+"""
+('img_init', img_init),
+('img_cur', img_cur),
+('img_predict', img_predict),
+('lnmk_cur', lnmk_cur),
+('lnmk_predict', lnmk_predict)])
+"""
+def gen_out_frame(imgs, word):
+    init = imgs['img_init'].copy()
+    gt = np.vstack((imgs['img_cur'].copy(), imgs['lnmk_cur'].copy()))
+    pred = np.pad(np.vstack((imgs['img_predict'].copy(), imgs['lnmk_predict'].copy())),
+                  pad_width=((0, 0), (0, 10), (0, 0)), mode='constant', constant_values=0)
+
+    compare = np.hstack((pred,gt))
+
+    padb = int(init.shape[0])
+
+    padded_init = np.pad(init, pad_width=((0,padb),(0,10),(0,0)), mode='constant', constant_values=0)
+
+    result = np.pad(np.hstack((padded_init,compare)), pad_width=((64,0),(0,0),(0,0)),
+                    mode='constant', constant_values=0)
+    # Put Word
+    text_loc = (4, 256)
+    cv2.putText(result, word, text_loc, cv2.FONT_HERSHEY_SIMPLEX,
+                thickness=2, color=(255, 255, 255),  fontScale=1.3) \
+
+    # Put Init label
+    gt_loc = (18, 40)
+    cv2.putText(result, "Initial", gt_loc, cv2.FONT_HERSHEY_SIMPLEX,
+                thickness=1, color=(255, 255, 255),  fontScale=1)
+    # Put Predict label
+    gt_loc = (148, 40)
+    cv2.putText(result, "Predict", gt_loc, cv2.FONT_HERSHEY_SIMPLEX,
+                thickness=1, color=(255, 255, 255),  fontScale=1)
+
+    # Put GT label
+    pred_loc = (320, 40)
+    cv2.putText(result, "GT", pred_loc, cv2.FONT_HERSHEY_SIMPLEX,
+                thickness=1, color=(255, 255, 255), fontScale=1)
+
+    return result
 
 
 if __name__ == '__main__':
@@ -50,6 +92,7 @@ if __name__ == '__main__':
     web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
 
+    out_frames = []
     # test
     for vid_idx, video in enumerate(dataset):
         vid_start_time = time.time()
@@ -58,7 +101,6 @@ if __name__ == '__main__':
             break
         init_tensor = True
         vid_path = "./output/test/test_{0}.mp4".format(vid_idx)
-        writer = skvideo.io.FFmpegWriter(vid_path)
         last_word = None
 
         for frame_idx, frame in enumerate(video):
@@ -110,7 +152,6 @@ if __name__ == '__main__':
             pred_frame = model.test(init_tensor, wordChage=wordChange)
             init_tensor=False
 
-            writer.writeFrame(pred_frame)
             errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
             visualizer.print_current_errors(vid_idx, frame_idx, errors, t, t_data)
@@ -120,9 +161,14 @@ if __name__ == '__main__':
 
             # Set up next frame
             last_word = word
+            visuals = model.get_current_visuals()
+            out_frames.append(gen_out_frame(visuals, word))
 
-        writer.close()
-        visuals = model.get_current_visuals()
+        #outputdata = np.expand_dims(np.array(out_frames), axis=3)
+        outputdata = np.array(out_frames)
+        skvideo.io.vwrite(vid_path.format(vid_idx), outputdata)
+
+        #writer.close()
         #img_path = model.get_image_paths()
         print('%04d: process video... %s' % (vid_idx, vid_path))
         #visualizer.save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio)
