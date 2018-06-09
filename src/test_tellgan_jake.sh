41eabdf25f15311f4d0ccb0de8f2f0b26ed32db7
Jake <spacejake@gmail.com>
2018-06-09 12:32:30 +0900

    missed some conflicts

--- src/test_tellgan_jake.sh
+++ src/test_tellgan_jake.sh
@@ -1,7 +1,4 @@
 #CUDA_VISIBLE_DEVICES=2 python train.py --dataroot /data2/victorleee/grid/	 --name tellgan_ver1 --model tell_gan --continue_train
 
-<<<<<<< HEAD
 #python test.py --dataroot /home/jake/classes/cs703/Project/data/grid_test/ --name tellgan_ver1 --model tell_gan  --how_many 2
-=======
->>>>>>> 57b94183fdda84d12e8a36060211d08f417f161f
 python test.py --dataroot /home/jake/classes/cs703/Project/data/grid_test/ --name tellgan_ver1 --model tell_gan  --how_many 2
